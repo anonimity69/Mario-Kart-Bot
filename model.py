26209900f9c08e000ca88eda5284ceb8ec9ff306
anonimity69 <shrayanmandal21@gmail.com>
2025-05-17 11:32:21 +0530

    model creation

--- model.py
+++ model.py
@@ -1,6 +1,17 @@
+import torch
+import torch.nn as nn   
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
 class DQN:
-    def __init__(self):
-         pass
+    '''A simple DQN model for reinforcement learning'''
+    def __init__(self, input_dim, output_dim):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, output_dim)
     def forward(self, x):
-         pass
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        return self.fc3(x)
     
